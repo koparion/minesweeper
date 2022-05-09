63613b0251d0be0cf0b9641897f7cef7eb5bc6fc
Lynette Li <huan.li@cix.csi.cuny.edu>
2022-05-09 00:18:35 -0400

    timer

---
+++ ClockPane.java
@@ -0,0 +1,34 @@
+package application;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class ClockPane extends Pane {
+	
+	       //declare variable
+          int time = 0;
+	
+          //constructor
+	public ClockPane() {
+		setCurrentTime();
+	}
+	
+	public void setCurrentTime() {
+		time++;
+		paintClock();
+	}
+	
+	protected void paintClock() {
+		Label label = new Label(String.valueOf(time));
+		label.setTextFill(Color.web("#0076a3"));
+		label.setFont(Font.font("Arial", 50));
+		getChildren().clear();
+		getChildren().add(label);
+	}
+	
+	public int getTime() {
+		return time;
+	}
+}
