aa4836359dfd41bd06b558e77d7e3bc694a813b6
rus <kipnis.rus@gmail.com>
2022-05-12 20:04:38 -0400

    added image coded to be implemented inside a method

---
+++ src/application/Images.java
@@ -0,0 +1,12 @@
+package application;
+
+import javafx.scene.image.*;
+
+public class Images {
+
+		Image bomb = new Image("file:bomb.png");
+		Image flag = new Image("file:flag.png");
+		
+		ImageView viewBomb = new ImageView(bomb);
+		ImageView viewFlag = new ImageView(flag);
+}
